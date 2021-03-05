bc0773b7fefd6489282602ebed8bbba59584f579
Brian Callahan <b2callahan@gmail.com>
2021-03-05 14:26:30 -0500

    test

--- src/banking/Driver.java
+++ src/banking/Driver.java
@@ -7,80 +7,43 @@ public class Driver {
 	
 	public static void main(String[] args) {
 		/*
-		System.out.println("Welcome to the personal banking app");
+		
 		System.out.println("Would you like to login/register: Y or N?");
 		Scanner scanner = new Scanner(System.in);			
 		String welcome = scanner.nextLine();
-		
-		if (welcome.equalsIgnoreCase("y")) {
-			System.out.println("Do you have an account with us?: Y or N?");
-			String account = scanner.nextLine();
-			if (welcome.equalsIgnoreCase("y")) {
-				System.out.println("Please enter your details");
-			}
-			if (welcome.equalsIgnoreCase("n")) {
-				System.out.println("click here to register");
-			}
-		}
-		if (welcome.equalsIgnoreCase("n")) {
-			System.out.println("See you later");
-		}
-		//System.out.println(user);
-		 
-		 */
-		/*
-		Scanner scanner = new Scanner(System.in);	
-		User use = new User();
-		AccountBalance acc = new AccountBalance();
-		List<User> userList = new ArrayList<>();
-		System.out.println("Create an account");
-		System.out.println("Please enter a username");
-		String username = scanner.nextLine();
-		
-		use.setUsername(username);
-		
-		System.out.println("Please enter a password");
-		String password = scanner.nextLine();
-		
-		use.setPassword(password);
-		use.setAccountId(1001);
-		acc.setAccountId(1001);
-		System.out.println("You have been assigned Account Id: "+ use.getAccountId());
-		System.out.println("Initial balance: set an amount to deposit");
-		String amount = scanner.nextLine();
-		acc.setBalance(Integer.valueOf(amount));
-		
-		userList.add(use);
-		
-		System.out.println(use.getUsername()+" "+use.getPassword()+" "+acc.getBalance());
-		
-		
-		for(User x : userList) {
-			System.out.println(x.toString());
-		}
 		*/
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Register an account");
-		System.out.println("Enter a username");
-		String username = scanner.next();
-		System.out.println("Enter a password");
-		String password = scanner.next();
-			
 		
-		User user = new User(username,password);
-		UserList userlist = new UserList();
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Welcome to the personal banking app");
+		System.out.println("Would you like to login or register?");
+		String welcome = scanner.nextLine();
+		if (welcome == "login") {
+			
+		} else if (welcome.equals("register")) {
+			System.out.println("Register an account");
+			System.out.println("Enter a username");
+			String username = scanner.next();
+			System.out.println("Enter a password");
+			String password = scanner.next();
 				
-		try {
-			userlist.createUser(user);
-		} catch (UserNameTaken e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			
+			User user = new User(username,password);
+			UserList userlist = new UserList();
+					
+			try {
+				userlist.createUser(user);
+			} catch (UserNameTaken e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			Account acc = new Account(user);
+			Menu menu = new Menu();
+			
+			menu.menu(user);
+			
 		}
 		
-		Account acc = new Account(user);
-		Menu menu = new Menu();
-		
-		menu.menu(user);
 		System.out.println("Ended");
 		
 	}
